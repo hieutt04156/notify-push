718e3196543f4b193fc6f5c60c39ffb429cb5986
Nguyen Thang <thangn.1411@gmail.com>
2015-04-23 09:24:57 +0700

    Update Notification.php

--- src/Notification.php
+++ src/Notification.php
@@ -1,1 +1,13 @@
+<?php 
+namespace Adayroi\NotificationPush;
 
+use Adayroi\NotificationPush\Facedes\Connection;
+class Notification {
+  
+  public $connection;
+  
+  public function __construct() 
+  {
+    $this->connection = Connection::getConnection();
+  }
+}

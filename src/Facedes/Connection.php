abd623d60488c636a2df0c2b57b19b910831a58c
Nguyen Thang <thangn.1411@gmail.com>
2015-04-23 09:18:21 +0700

    Update Connection.php

--- src/Facedes/Connection.php
+++ src/Facedes/Connection.php
@@ -1,1 +1,13 @@
+<?php
+namespace Adayroi\NotificationPush\Facedes;
 
+use Illuminate\Support\Facades\Facade as IlluminateFacade;
+
+class Connection extends IlluminateFacade 
+{
+  protected static function getFacadeAccessor()
+  {
+      return 'noticationpush';
+  }
+}
+?>

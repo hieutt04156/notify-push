77e27841481e5407845dc338f3af55ca28b53308
Nguyen Thang <thangn.1411@gmail.com>
2015-04-23 08:36:19 +0700

    Update and rename notificationServiceProvider.php to NotificationPushServiceProvider.php

---
+++ src/NotificationPushServiceProvider.php
@@ -0,0 +1,22 @@
+<?php 
+namespace Adayroi\NotificationPush;
+
+
+class NotificationPushServiceProvider 
+{
+  
+  
+  public function boot()
+  {
+    $this->package('adayroi/notificationpush');
+  }
+  
+  public function register() 
+  {
+    $this->app->bindShare('notificationpush', function($app){
+      $config = $app->config->get('notificationpush::config');
+      return new Notification($config);
+    });
+  }
+  
+}
